cfe758327fb649c8c01b4621a8be2f167de20cdf
Ruixin Zhang <zhangruixin516@gmail.com>
2023-11-15 21:15:18 -0500

    Complete some UI, complete database setting

--- Diet_Exercise_Journal/src/DatabaseOperation/RuntimeDatabase.java
+++ Diet_Exercise_Journal/src/DatabaseOperation/RuntimeDatabase.java
@@ -2,13 +2,17 @@ package DatabaseOperation;
 
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class RuntimeDatabase {
     private Connection connect = null;
     private Statement statement = null;
-
     private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
+
+
+    private int id;
+
 
     /**
      * This class display all profiles in the table (database)
@@ -21,7 +25,20 @@ public class RuntimeDatabase {
             statement = connect.createStatement();
             resultSet = statement.executeQuery("select * from Diet_Exercise_Journal_UserProfile.UserProfile");
             //上面4行只要创新方法就要重新加上
-            displayResultSet(resultSet);
+            while (resultSet.next()) {
+                // It is possible to get the columns via name
+                // also possible to get the columns via the column number
+                // which starts at 1
+
+                System.out.println("UserID：" + resultSet.getInt(1)); //  获取第一列的数据
+                System.out.println("UserName：" + resultSet.getString("UserName"));  //获取字段为name的数据
+                System.out.println("Sex：" + resultSet.getString(3)); //  获取第3列的数据
+                System.out.println("DOB：" + resultSet.getDate(4)); //  获取第4列的数据
+                System.out.println("Height：" + resultSet.getDouble(5)); //  获取第5列的数据
+                System.out.println("Weight：" + resultSet.getDouble(6)); //  获取第6列的数据
+                System.out.println("Measurement：" + resultSet.getString(7)); //  获取第7列的数据
+                System.out.println("-----------------------------------------------");
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,30 +48,54 @@ public class RuntimeDatabase {
         }
     }
 
-    /**
-     * This method print the table
-     * @param resultSet is the table (database)
-     * @throws SQLException if sql error
-     */
-    private void displayResultSet(ResultSet resultSet) throws SQLException {
-        // ResultSet is initially before the first data set
-        while (resultSet.next()) {
-            // It is possible to get the columns via name
-            // also possible to get the columns via the column number
-            // which starts at 1
 
-            System.out.println("UserID：" + resultSet.getInt(1)); //  获取第一列的数据
-            System.out.println("UserName：" + resultSet.getString("UserName"));  //获取字段为name的数据
-            System.out.println("Sex：" + resultSet.getString(3)); //  获取第3列的数据
-            System.out.println("DOB：" + resultSet.getDate(4)); //  获取第4列的数据
-            System.out.println("Height：" + resultSet.getDouble(5)); //  获取第5列的数据
-            System.out.println("Weight：" + resultSet.getDouble(6)); //  获取第6列的数据
-            System.out.println("Measurement：" + resultSet.getString(7)); //  获取第7列的数据
-            System.out.println("-----------------------------------------------");
+    public void displayDietData(int userID) throws Exception{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/Diet_Exercise_Journal_UserProfile", "root", "zxcv6509");
+            statement = connect.createStatement();
+            resultSet = statement.executeQuery("select Date from Diet_Exercise_Journal_UserProfile.Meal where UserID = " + String.valueOf(userID));
+            resultSet.next();
+            Date date = resultSet.getDate(1);
+
+            resultSet = statement.executeQuery("select Ingredients, Quantity, CalorieIntake from Diet_Exercise_Journal_UserProfile.Breakfast where Date = '" + date + "'");
+            resultSet.next();
+            System.out.println(resultSet.getString(1));
+            System.out.println(resultSet.getString(2));
+            System.out.println(resultSet.getDouble(3));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            close();
         }
     }
 
+    public void displayProfile() throws Exception{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/Diet_Exercise_Journal_UserProfile", "root", "zxcv6509");
+            statement = connect.createStatement();
+            resultSet = statement.executeQuery("select * from Diet_Exercise_Journal_UserProfile.UserProfile");
 
+            int i = 1;
+            while(resultSet.next()){
+                System.out.println(i + ": " + resultSet.getString("UserName") + " (id: " + resultSet.getInt(1) + ")");
+                i++;
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            close();
+        }
+    }
+    public ArrayList<Double> getNutrientValue(ArrayList<String> food){
+        return null;
+    }
     /**
      * This class close the stream
      */
@@ -76,6 +117,58 @@ public class RuntimeDatabase {
         }
     }
 
+//-----------------------------------------------------------------------------------------------------------
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+//-----------------------------------------------------------------------------------------------------------
+    /**
+     * This class create a profile for user and store it to the database
+     * @param UserName is the username
+     * @param sex is the sex
+     * @param year is the year of birth
+     * @param month is the month of birth
+     * @param day is the day of birth
+     * @param height is the height in meter
+     * @param weight is the weight in kg
+     * @param measurement is the measurement the user want
+     * @throws Exception if sql command has syntax error
+     */
+    public void createProfile(String UserName, String sex, int year, int month, int day, double height, double weight, String measurement) throws Exception{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/Diet_Exercise_Journal_UserProfile", "root", "zxcv6509");
+            statement = connect.createStatement();
+            resultSet = statement.executeQuery("select * from Diet_Exercise_Journal_UserProfile.UserProfile");
+
+            //以下为创建profile
+            preparedStatement = connect.prepareStatement("insert into Diet_Exercise_Journal_UserProfile.UserProfile values (default, ?, ?, ?, ?, ?, ?)");
+            //preparedStatement.setInt(1, ++id);
+            preparedStatement.setString(1, UserName);
+            preparedStatement.setString(2, sex);
+            preparedStatement.setString(3, year + "-" + month + "-" + day);
+            preparedStatement.setDouble(4, height);
+            preparedStatement.setDouble(5, weight);
+            preparedStatement.setString(6, measurement);//metric vs imperial
+            preparedStatement.executeUpdate();
+
+            preparedStatement = connect.prepareStatement("SELECT UserID, UserName, Sex, Date_of_birth, Height, Weight, Measurement from Diet_Exercise_Journal_UserProfile.UserProfile");
+            resultSet = preparedStatement.executeQuery();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            close();
+        }
+    }
     /**
      * This method reads the calorie burnt based on the date
      * @return an array in [date][data], [date][data]...
--- Diet_Exercise_Journal/src/Main_Classes/MainUI.java
+++ Diet_Exercise_Journal/src/Main_Classes/MainUI.java
@@ -1,54 +1,61 @@
 package Main_Classes;
 
-import DatabaseOperation.ProfileOperator;
 import DatabaseOperation.RuntimeDatabase;
-
-import java.util.Scanner;
 
 /**
  * This class for design main GUI
  * Not implemented GUI yet
  */
+//测试用
 public class MainUI {
     public static void main(String[] args) throws Exception {
-        System.out.println("Here are the profiles you have: ");
-        Scanner sc = new Scanner(System.in);
 
-        ProfileOperator profileOperator = new ProfileOperator();
-        profileOperator.displayProfile();
-        System.out.println("Choose a profile or you want to create a new one: choose the number, or 0 to create");
-        String input = sc.next();
+        RuntimeDatabase runtimeDatabase = new RuntimeDatabase();
+        runtimeDatabase.displayDietData(100001);
 
-        String UserName;
-        String sex;
-        int year;
-        int month;
-        int day;
-        double height;
-        double weight;
-        String measurement;
-        //create profile
-        if(input.equals("0")){
-            System.out.println("Please enter the username: ");
-            UserName = sc.nextLine();
-            UserName += sc.nextLine();
-            System.out.println("Please enter your sex (Male/Female): ");
-            sex = sc.next();
-            System.out.println("Please enter your date of birth:\nEnter year: ");
-            year = sc.nextInt();
-            System.out.println("Enter month: ");
-            month = sc.nextInt();
-            System.out.println("Enter day: ");
-            day = sc.nextInt();
-            System.out.println("Please enter your height in meter (e.g. 1.83): ");
-            height = sc.nextDouble();
-            System.out.println("Please enter your weight in kg (e.g. 70): ");
-            weight = sc.nextDouble();
-            System.out.println("Please enter the measurement you want (metric/imperial): ");
-            measurement = sc.next();
+        runtimeDatabase.readDatabase();
+        runtimeDatabase.displayProfile();
 
-            profileOperator.createProfile(UserName, sex, year, month, day, height, weight, measurement);
-        }
+
+
+//        System.out.println("Here are the profiles you have: ");
+//        Scanner sc = new Scanner(System.in);
+//
+//        ProfileOperator profileOperator = new ProfileOperator();
+//        profileOperator.displayProfile();
+//        System.out.println("Choose a profile or you want to create a new one: choose the number, or 0 to create");
+//        String input = sc.next();
+//
+//        String UserName;
+//        String sex;
+//        int year;
+//        int month;
+//        int day;
+//        double height;
+//        double weight;
+//        String measurement;
+//        //create profile
+//        if(input.equals("0")){
+//            System.out.println("Please enter the username: ");
+//            UserName = sc.nextLine();
+//            UserName += sc.nextLine();
+//            System.out.println("Please enter your sex (Male/Female): ");
+//            sex = sc.next();
+//            System.out.println("Please enter your date of birth:\nEnter year: ");
+//            year = sc.nextInt();
+//            System.out.println("Enter month: ");
+//            month = sc.nextInt();
+//            System.out.println("Enter day: ");
+//            day = sc.nextInt();
+//            System.out.println("Please enter your height in meter (e.g. 1.83): ");
+//            height = sc.nextDouble();
+//            System.out.println("Please enter your weight in kg (e.g. 70): ");
+//            weight = sc.nextDouble();
+//            System.out.println("Please enter the measurement you want (metric/imperial): ");
+//            measurement = sc.next();
+//
+//            profileOperator.createProfile(UserName, sex, year, month, day, height, weight, measurement);
+//        }
 
     }
 }
