7fe6eb5289e60cb166e77ea7fc62b1468dea07e6
david Dai <ddai2002@my.yorku.ca>
2023-11-19 16:23:42 -0500

    2023/11/16 by DD

--- Diet_Exercise_Journal/src/OutcomeGenerator/CFG.java
+++ Diet_Exercise_Journal/src/OutcomeGenerator/CFG.java
@@ -50,7 +50,6 @@ public class CFG extends JFrame {
         SwingUtilities.invokeLater(() -> {
             setSize(800, 600);
             setLocationRelativeTo(null);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setVisible(true);
         });
     }
